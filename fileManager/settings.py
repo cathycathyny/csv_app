a0bdb9101503758ebf27e1d53a8a31647aa55442
cathycathyny <cathylsq1@gmail.com>
2020-10-03 17:02:40 -0400

    init csvAPp

--- fileManager/settings.py
+++ fileManager/settings.py
@@ -30,7 +30,7 @@ ALLOWED_HOSTS = []
 
 # Application definition
 
-INSTALLED_APPS = [
+DJANGO_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -38,6 +38,13 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 ]
+
+APPS = [
+    'csvApp',
+]
+
+INSTALLED_APPS = APPS + DJANGO_APPS
+
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
